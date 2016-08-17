fa345822cc09a88cb60ab1b444a593dae1291b9d
ivan <ivanchen@moneydj.com>
2016-08-17 11:32:08 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Counter example
+
+Simple example to get your started with React-Native and Redux
+
+"react-native": "0.31.0"
---
+++ index.android.js
@@ -0,0 +1,8 @@
+'use strict';
+import React, { Component } from 'react';
+import { AppRegistry,View } from 'react-native';
+
+//import React, { AppRegistry } from 'react-native';
+import App from './app/containers/app';
+
+AppRegistry.registerComponent('DJNews', () => App);
